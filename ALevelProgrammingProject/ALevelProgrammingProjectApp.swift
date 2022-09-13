41a21cc8e5848908ba1c0aa3516442d0d2204a60
Nick Hodson <hodson.n@etoncollege.org.uk>
2022-09-13 09:13:41 +0100

    Initial Commit

---
+++ ALevelProgrammingProject/ALevelProgrammingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALevelProgrammingProjectApp.swift
+//  ALevelProgrammingProject
+//
+//  Created by Nicholas Hodson on 13/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ALevelProgrammingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
