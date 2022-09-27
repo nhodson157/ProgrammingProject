d72e44763c77abfef3f66e25c1f95d9eece97191
Nick Hodson <hodson.n@etoncollege.org.uk>
2022-09-27 10:32:00 +0100

    Rewards view

--- ALevelProgrammingProject/Views/MainView.swift
+++ ALevelProgrammingProject/Views/MainView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct MainView: View {
-    @State private var randomText: String = ""
+    @State private var checked = false
     
     var body: some View {
             ZStack{
@@ -60,8 +60,38 @@ struct MainView: View {
 
                     }
                     VStack{
+                        Text("Chore 1              ")
+                            .font(.largeTitle)
+                            .fontWeight(.medium)
                         
-                    }.padding()
+                        Text("\n\nDescription about the chore, any information given by the parent user \n\n")
+
+                        
+                        HStack {
+                            CheckBoxView(checked: $checked)
+                            Text("Completed?")
+                        }.padding()
+                        
+                        HStack{
+                            Text("Reward: ")
+                            Text("100")
+                            Image(systemName: "star.fill")
+                        }.padding()
+                        
+                        HStack{
+                            Text("Due by:")
+                            Text("29/11/22")
+                                .padding(5)
+                                .border(.primary)
+                            Image(systemName: "calendar")
+                        }.padding()
+                        
+                        HStack{
+                            Text("Set by: ")
+                            Text("Mother")
+                        }
+                    }
+                    .padding()
                         .border(.primary)
                 }
         }.padding(15)
@@ -71,6 +101,8 @@ struct MainView: View {
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
         MainView()
+            
+        
     }
 }
 
--- ALevelProgrammingProject/Views/RewardsView.swift
+++ ALevelProgrammingProject/Views/RewardsView.swift
@@ -9,7 +9,53 @@ import SwiftUI
 
 struct RewardsView: View {
     var body: some View {
-        Text("Rewards")
+        ZStack{
+            VStack{
+                HStack{
+                    Text("Rewards")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                    Spacer()
+                    Button(action: {}) {
+                        Image(systemName: "info.circle.fill")
+                            .resizable(resizingMode: .stretch)
+                            .foregroundColor(Color.black)
+                        .frame(width: 35.0, height: 35.0)}
+                    .padding(.trailing)
+                }.padding()
+                
+                HStack{
+                    Spacer()
+                    Text("Your Balance: ")
+                        .font(.body)
+                        .fontWeight(.medium)
+                    Text("215")
+                        .font(.title)
+                    Image(systemName: "star.fill")
+                }.padding(20)
+                
+                ScrollView{
+                    GroupBox(label: Label("Reward 1", systemImage: "gift")){
+                        Text("Reward 1")
+                    }
+                    .cornerRadius(20)
+                    GroupBox(label: Label("Reward 2", systemImage: "gift")){
+                        Text("Reward 2")
+                    }
+                    .cornerRadius(20)
+                    GroupBox(label: Label("Reward 3", systemImage: "gift")){
+                        Text("Reward 3")
+                    }
+                    .cornerRadius(20)
+                    GroupBox(label: Label("Reward 4", systemImage: "gift")){
+                        Text("Reward 4")
+                    }
+                }
+                
+                Spacer()
+                
+            }
+        }
     }
 }
 
