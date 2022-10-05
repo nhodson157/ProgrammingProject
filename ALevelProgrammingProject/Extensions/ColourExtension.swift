198f17dab263d0f6a87943fa5ef3579695b1834b
Nick Hodson <hodson.n@etoncollege.org.uk>
2022-10-05 11:21:44 +0100

    Colour palette added

---
+++ ALevelProgrammingProject/Extensions/ColourExtension.swift
@@ -0,0 +1,15 @@
+//
+//  ColourExtension.swift
+//  ALevelProgrammingProject
+//
+//  Created by Nicholas Hodson on 05/10/2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let alabaster = Color("Alabaster")
+    static let pastelGray = Color("Pastel gray")
+    static let pewterBlue = Color("Pewter blue")
+}
--- ALevelProgrammingProject/Views/RewardsView.swift
+++ ALevelProgrammingProject/Views/RewardsView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct RewardsView: View {
     var body: some View {
         ZStack{
+            Rectangle()
+            Color.pewterBlue
             VStack{
                 HStack{
                     Text("Rewards")
