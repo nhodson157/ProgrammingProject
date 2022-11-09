2733423db59edb928ef7acfe5bd3b7b08a7f1bc1
Nick Hodson <hodson.n@etoncollege.org.uk>
2022-11-09 11:20:52 +0000

    saving...

--- ALevelProgrammingProject/Model/Chore.swift
+++ ALevelProgrammingProject/Model/Chore.swift
@@ -12,25 +12,32 @@ class Chore {
     let dueBy: String
     let description: String
     let setBy: String
-    let reward: Int
+    let difficulty: Int
     let completed: Bool
+    var reward: Int
     
-    init(name: String, dueBy: String, description: String, setBy: String, reward: Int, completed: Bool) {
+    init(name: String, dueBy: String, description: String, setBy: String, difficulty: Int, completed: Bool) {
         self.name = name
         self.dueBy = dueBy  //Initialising the class and attributes
         self.description = description
         self.setBy = setBy
-        self.reward = reward
+        self.difficulty = difficulty
         self.completed = completed
+        self.reward = 0
     }
     
     
     func calcReward() {
+        var temp = 100 * self.difficulty
+        if self.name
         
+        
+        
+        return temp
     }
     
     
-    static let example = Chore(name: "Example Chore", dueBy: "11th October 2022", description: "Example chore description", setBy: "Myself", reward: 100, completed: false)
+    static let example = Chore(name: "Example Chore", dueBy: "11th October 2022", description: "Example chore description", setBy: "Myself", difficulty: 5, completed: false)
     //Creating an example instance of the chore class
     
 }
